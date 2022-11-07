4c4297fae02f3f003d37be5ee3b2e85657571fda
Jamienorthman <mikoann88@gmail.com>
2022-11-07 22:09:44 +0100

    currently showing words that are only euphemisms changed h1 changed text and background color created word list (array) made for loop for 'related words' or words with at least one of the same tags added mouseover event to display related words (colored green) added mouseout event to 'unhighlight' words unified styles for all words (bold, off-white) added words to word list

--- app.js
+++ app.js
@@ -3,17 +3,110 @@
 
 let wordList = [
     {
-        word: "utilize",
-        reason: "use!",
-        tags: ["unnecessary", "overused"],
+        word: "extraordinary rendition",
+        reason: "kidnapping",
+        styleCSS: "font-weight: bold",
+        tags: ["political", "war"],
         link: ""
     },
     {
         word: "bulk collection",
         reason: "mass surveillance",
-        tags: ["euphemism", "political"],
+        styleCSS: "font-weight: bold",
+        tags: ["security", "political"],
         link: "https://www.deutschlandfunk.de/dlf-interview-with-edward-snowden-what-would-a-society-be-100.html"
-    }
+    },
+    {
+        word: "enhanced interogation",
+        reason: "torture",
+        styleCSS: "font-weight: bold",
+        tags: ["political", "war"],
+        link: ""
+    },
+    {
+        word: "footfall measurement",
+        reason: "location tracking",
+        styleCSS: "font-weight: bold",
+        tags: ["capitalism", "data analytics"],
+        link: "https://vimeo.com/425137575"
+    },
+    {
+        word: "Psychographic breakdown",
+        reason: "personality labeling",
+        styleCSS: "font-weight: bold",
+        tags: ["capitalism", "data analytics"],
+        link: "https://vimeo.com/425137575"
+    },
+    {
+        word: "collateral damage",
+        reason: "massive civilian destruction during war",
+        styleCSS: "font-weight: bold",
+        tags: ["war", "mass media"],
+        link: "https://vimeo.com/425137575"
+    },
+    {
+        word: "casualties",
+        reason: "deaths",
+        styleCSS: "font-weight: bold",
+        tags: ["war", "mass media"],
+        link: ""
+    },
+    {
+        word: "counterintelligence",
+        reason: "defense against spying",
+        styleCSS: "font-weight: bold",
+        tags: ["security", "political"],
+        link: ""
+    },
+    {
+        word: "outsourcing",
+        reason: "whoever does it cheapest gets the job",
+        styleCSS: "font-weight: bold",
+        tags: ["capitalism", "labor"],
+        link: ""
+    },
+    {
+        word: "social distancing",
+        reason: "physical distancing",
+        styleCSS: "font-weight: bold",
+        tags: ["mass media", "pandemic"],
+        link: ""
+    },
+    {
+        word: "front line",
+        reason: "military line closest to enemy or influential position in a debate",
+        styleCSS: "font-weight: bold",
+        tags: ["war", "pandemic"],
+        link: ""
+    },
+    {
+        word: "Pushback",
+        reason: "forcing refugees or migrants back over a border",
+        styleCSS: "font-weight: bold",
+        tags: ["geopolitical", "immigration"],
+        link: ""
+    },
+    {
+        word: "downsizing",
+        reason: "layoffs",
+        styleCSS: "font-weight: bold",
+        tags: ["capitalism", "labor"],
+        link: ""
+    },
+    {
+        word: "subprime loan",
+        reason: "impossible to pay back",
+        styleCSS: "font-weight: bold",
+        tags: ["capitalism", "finance"],
+        link: ""
+    },
+    {
+        word: "ethnic cleansing",
+        reason: "forced ethnic homogeny",
+        styleCSS: "font-weight: bold",
+        tags: ["racism", "genocide"],
+        link: ""
+    },
 ];
 
 
@@ -21,21 +114,21 @@ let wordList = [
 console.log("CREATING ELEMENTS!")
 
 
-function appendWidl(word, reason, styleCSS, link) {
+function appendWidl(wordListEntry) {
     let containerDiv = document.getElementById("word-container");
 
     let widlDiv = document.createElement("div");
     widlDiv.classList.add("tooltip");
 
     let wordElement;
-    if (link) {
+    if (wordListEntry.link) {
         wordElement = document.createElement("a");
-        wordElement.href = link;
+        wordElement.href = wordListEntry.link;
         wordElement.target = "_blank"
     } else {
         wordElement = document.createElement("span");
     }
-    wordElement.style = styleCSS;
+    wordElement.style = wordListEntry.styleCSS;
     let toolTipSpan = document.createElement("span");
 
     toolTipSpan.classList.add("tooltiptext");
@@ -43,62 +136,116 @@ function appendWidl(word, reason, styleCSS, link) {
     widlDiv.appendChild(wordElement);
     widlDiv.appendChild(toolTipSpan);
 
-    wordElement.innerText = word;
-    toolTipSpan.innerText = reason;
+    wordElement.innerText = wordListEntry.word;
+    toolTipSpan.innerText = wordListEntry.reason;
+
+    wordListEntry.wordElementHTML = wordElement; 
 
     containerDiv.appendChild(widlDiv)
 }
 
-appendWidl("Utilize", "use!", "color:red")
+for (let i = 0; i < wordList.length; i++) {
+    let wordListEntry = wordList[i];
+    // creates the HTML divs and attached a reference to each word's html element to the word list entries
+    appendWidl(wordListEntry);
+}
 
-appendWidl("Juxtapose", "compare and contrast?", "font-weight:bold")
+for (let i = 0; i < wordList.length; i++) {
+    let wordListEntry = wordList[i];
+    let relatedWords = [];
+    // For each tag of this word...
+    for (let j = 0; j < wordListEntry.tags.length; j++) {
+        let wordTag = wordListEntry.tags[j];
+        // ...search all other words...
+        for (let k = 0; k < wordList.length; k++) {
+            let otherWordListEntry = wordList[k];
+            // ...to compare their tags with the given tag of this word.
+            // ** Could use array.includes() in the future **
+            for (let l = 0; l < otherWordListEntry.tags.length; l++) {
+                let otherWordTag = otherWordListEntry.tags[l];
+                // If the words are sharing at least 1 tag we consider them related
+                if (wordTag === otherWordTag) {
+                    relatedWords.push(otherWordListEntry);
+                    break;
+                }
+            }
+        }          
+    }
+    let wordHtml = wordListEntry.wordElementHTML;
+    // Highlight Event:
+    wordHtml.addEventListener("mouseover", (event) => {
+        for (let j = 0; j < relatedWords.length; j++) {
+            relatedWords[j].wordElementHTML.style.color = 'limeGreen';
+        }
+    });
+    // Unhighlight Event:
+    wordHtml.addEventListener("mouseout", (event) => {
+        for (let j = 0; j < relatedWords.length; j++) {
+            relatedWords[j].wordElementHTML.style.color = 'rgb(187, 175, 175)';
+        }
+    });
+}
 
-appendWidl("Bulk collection", "mass surveillance", "", "https://www.deutschlandfunk.de/dlf-interview-with-edward-snowden-what-would-a-society-be-100.html")
 
-appendWidl("conjouring", "produce or evoke", "color:gold; font-size:200%;")
+// appendWidl("Utilize", "use!", "color:red")
 
-appendWidl("Meandering", "weaving or move through?", "font-style:italic")
+// appendWidl("Juxtapose", "compare and contrast?", "font-weight:bold")
 
-appendWidl("extraordinary rendition", "kidnapping")
+/* appendWidl("Bulk collection", "mass surveillance", "font-weight:bold;", "https://www.deutschlandfunk.de/dlf-interview-with-edward-snowden-what-would-a-society-be-100.html")
+
+// appendWidl("conjouring", "produce or evoke", "color:gold; font-size:200%;")
+
+// appendWidl("Meandering", "weaving or move through?", "font-style:italic")
+
+appendWidl("extraordinary rendition", "kidnapping", "font-weight:bold")
 
 appendWidl("enhanced interrogation", "torture", "font-weight:bold")
 
-appendWidl("vignettes", "snapshots?", "font-style:italic")
+// appendWidl("vignettes", "snapshots?", "font-style:italic")
 
-appendWidl("methodologies", "methods")
+// appendWidl("methodologies", "methods")
 
-appendWidl("intermediaries", "mediator", "color:green")
+// appendWidl("intermediaries", "mediator", "color:green")
 
-appendWidl("individuating", "-__-", "font-weight:bold")
+// appendWidl("individuating", "-__-", "font-weight:bold")
 
-appendWidl("Footfall measurement", "location tracking", "", "https://vimeo.com/425137575")
+appendWidl("Footfall measurement", "location tracking", "font-weight:bold;", "https://vimeo.com/425137575")
 
-appendWidl("Psychographic breakdown", "personality labeling", "", "https://vimeo.com/425137575")
+appendWidl("Psychographic breakdown", "personality labeling", "font-weight:bold;", "https://vimeo.com/425137575")
 
-appendWidl("focalize", "just say focus", "font-weight:bold")
+// appendWidl("focalize", "just say focus", "font-weight:bold")
 
-appendWidl("collateral damage", "massive civilian destruction during war", "color:blue")
+appendWidl("collateral damage", "massive civilian destruction during war", "font-weight:bold;")
 
-appendWidl("Change-management", "can really manage change?!", "font-style:italic")
+// appendWidl("Change-management", "can really manage change?!", "font-style:italic")
 
-appendWidl("Sustainability", "long-term practices—nowadays used to gain profits", "font-size:200%; color:green;")
+// appendWidl("Sustainability", "long-term practices—nowadays used to gain profits", "font-size:200%; color:green;")
 
-appendWidl("trajectory", "path")
+// appendWidl("trajectory", "path")
 
-appendWidl("casualties", "deaths", "font-size:200%; color:purple; font-style:italic")
+appendWidl("casualties", "deaths", "font-weight:bold;")
 
-appendWidl("inflection point", "turning point/milestone", "font-style:italic")
+// appendWidl("inflection point", "turning point/milestone", "font-style:italic")
 
 appendWidl("counterintelligence", "defense against spying", "font-weight:bold;")
 
-appendWidl("outsourcing", "whoever does it cheapest gets the job", "font-size:150%")
+appendWidl("outsourcing", "whoever does it cheapest gets the job", "font-weight:bold;")
 
-appendWidl("Posthuman", "evolution")
+// appendWidl("Posthuman", "evolution")
 
-appendWidl("useless", "no need to be useful", "font-weight:bold;")
+// appendWidl("useless", "no need to be useful", "font-weight:bold;")
 
-appendWidl("Apropos", "relevant", "font-style:italic; color:blue;")
+// appendWidl("Apropos", "relevant", "font-style:italic; color:green;")
 
-appendWidl("social distancing", "physical distancing", "font-weight:bold; color:red;")
+appendWidl("social distancing", "physical distancing", "font-weight:bold;")
 
-appendWidl("front line", "military line closest to enemy or influential position in a debate", "color:gold; text-stroke:1px black; font-size:150%;")
+appendWidl("front line", "military line closest to enemy or influential position in a debate", "font-weight:bold;")
+
+appendWidl("Pushback", "forcing refugees or migrants back over a border", "font-weight:bold;")
+
+appendWidl("downsizing", "layoffs", "font-weight:bold;")
+
+appendWidl("subprime loan", "impossible to pay back", "font-weight:bold;")
+
+appendWidl("ethnic cleansing", "forced ethnic homogeny", "font-weight:bold;") */
+
